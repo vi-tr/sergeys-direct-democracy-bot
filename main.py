00e9f7c9b25621e532737f121d435fc3193702ed
Anton Franssen <antonfrantsen@gmail.com>
2024-03-26 20:29:44 +0200

    Какая-то ерунда

--- main.py
+++ main.py
@@ -3,12 +3,43 @@
 #tak chto naslagdaites' latinicei, po vozmognosti todge pishite komenti
 #tut importi
 import discord
+from discord import Intents, Client, Message
+from typing import *
 import os
+from dotenv import load_dotenv
+
+from discord import app_commands
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv('BOT_TOKEN')
+
+#инициализация бота должна находиться в глоабльной области видимости,
+#так как из main обьект класса Client никто не увидит
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 # Eto tochka vhoda
 def main():
-    client = discord.Client()
-    client.run(os.getenv("TOKEN"))
+    client.run(token=TOKEN)
+#Anton Franssen is here XD
+#26.03.2024
+
+#Не знаю почему, но без аргумента intents у меня ничего не работает :)
+#В любом случае, оно никак не должно помешать работе бота   
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def send(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    if(message.content == "Testing message"):
+        await message.channel.send("NO")
 
 # Eto todge tochka vhoda
 if __name__ == "__main__":
