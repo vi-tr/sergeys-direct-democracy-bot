c5c03837d67d8a64848ee83f4deb267cb189a05e
Kemran <109353270+remr2005@users.noreply.github.com>
2024-03-30 22:18:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-# Ia neznaiu kto kakoi imeet uroven' pintona poetomu inogda budu pisat' samie ochevidnie veshi
-#A eshe u menia sletela ruskaia raskladka
-#tak chto naslagdaites' latinicei, po vozmognosti todge pishite komenti
-#tut importi
+# импорты
 import discord
+from discord import Intents, Client, Message
+from discord.ext import commands
+from typing import *
 import os
+import asyncio
 
-# Eto tochka vhoda
-def main():
-    client = discord.Client()
-    client.run(os.getenv("TOKEN"))
+from discord import app_commands
 
-# Eto todge tochka vhoda
+# Токен передаем через .env файл, если хотите его узнать, пишите мне, Антону или Вите
+TOKEN: Final[str] = os.getenv('BOT_TOKEN')
+
+#инициализация бота должна находиться в глоабльной области видимости,
+#так как из main обьект класса Client никто не увидит
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(
+    command_prefix="/",
+    intents = intents
+)
+
+# здесь не нужно ничего трогать
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+# Ещё одна точка входа
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
+
+# Точка входа
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+
+# PS Писать свои функции, нужно в отдельных файлах в папке cogs 
