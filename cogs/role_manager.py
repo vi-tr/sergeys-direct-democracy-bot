2f1aae4bb8f900a6fce4bb2276ad865b90254402
Anton Franssen <AntonFranssen@yandex.ru>
2024-04-02 00:10:30 +0300

    Доделал роли

--- cogs/role_manager.py
+++ cogs/role_manager.py
@@ -1,27 +1,68 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import Cog
-from create_role import create_role
+# from create_role import create_role
+from discord import Permissions, Client, TextChannel, VoiceChannel, StageChannel, Colour, Role
 
-class RoleManager(discord.ext.commands.Сog):
+class CustomRole():
+    def __init__(self):
+        self.admin = Permissions.elevated()
+        self.moderator = Permissions(
+            kick_members=True, 
+            manage_channels=True,
+            moderate_members=True,
+            mute_members=True,
+            move_members=True
+        )
+        self.member = Permissions.membership()
+
+        self.perm_dict = {
+            "admin": self.admin,
+            "moderator": self.moderator,
+            "member": self.member
+        }
+
+    def get_role_permissions_factory(self, role_type : str) -> Permissions | bool:
+        if (not role_type in self.perm_dict):
+            return False
+        return self.perm_dict[role_type]
+
+
+class RoleManipulation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command(name='add_role')
-    async def add_role(self, ctx, role_perm, r, g, b, *role_name_args):
-        new_role = create_role(
-            self.bot,
-            ctx,
-            role_perm,
-            r, g, b,
-            role_name_args
-        )
 
-        if not new_role:
-            return False
-        else:
-            await ctx.guild.create_role(new_role)
-            await ctx.reply(f"Роль {role_name_args} была успешно создана!")
+    @commands.command(name='add_role')
+    async def create_role(
+            self,
+            ctx: TextChannel | VoiceChannel | StageChannel,
+            role_perm: str,
+            r, g, b,
+            *role_name_args):
+        
+        role_obj = CustomRole()
+        
+        role_name = ' '.join(role_name_args)
+        role_type = role_obj.get_role_permissions_factory(role_perm)
+
+        try:
+            if (not role_type):
+                await ctx.reply(f'Типа роли {role_type} не существует в списке возможных ролей!')
+
+    #вот это оставляет желать лучшего, лабы Кемрика на Go на меня плохо влияют
+            elif (int(r) > 255 or int(g) > 255 or int(b) > 255 or int(r) < 0 or int(g) < 0 or int(b) < 0):
+                await ctx.reply("Введен неверный формат цвета в RGB :(")
+
+            else:
+                role = ctx.guild.create_role(
+                name=role_name, 
+                color=discord.Colour.from_rgb(int(r), int(g), int(b)),
+                permissions=role_type
+                )
+                await role
+                await ctx.reply("Роль была создана!")
+                
+        except:
+            await ctx.reply("Вероятно, параметры комманды /add_role были введены в неверном порядке. Попробуйте снова.")
 
 async def setup(bot):
-    await bot.add_cog(RoleManager(bot))
+    await bot.add_cog(RoleManipulation(bot))
