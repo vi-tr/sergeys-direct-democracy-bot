86575ad751a3a6644fd8b00894af13bdaeef4163
AleksandrLisitsa <144948408+AleksandrLisitsa@users.noreply.github.com>
2024-04-02 15:26:07 +0300

    Add new_voice.py

---
+++ cogs/new_voice.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class SimpleCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='create_voice_channel')
+    async def create_voice_channel(self, ctx, category_name: str, channel_name: str):
+        """
+        Создает голосовой канал с заданным именем в заданной категории.
+        Пример использования: /create_voice_channel MyCategory MyVoiceChannel
+        """
+        guild = ctx.guild
+        category = discord.utils.get(guild.categories, name=category_name)
+        if category is None:
+            category = await guild.create_category(category_name)
+            await ctx.send(f"Категория '{category_name}' создана.")
+        
+        await guild.create_voice_channel(channel_name, category=category)
+        await ctx.send(f"Голосовой канал '{channel_name}' успешно создан в категории '{category_name}'!")
+
+async def setup(bot):
+    await bot.add_cog(SimpleCog(bot))
+
