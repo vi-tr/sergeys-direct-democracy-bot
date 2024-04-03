7a4ea17568e4ac6eeefe8f9cdfcaba92f631338a
Kemran <109353270+remr2005@users.noreply.github.com>
2024-04-03 11:01:44 +0300

    Add files via upload

---
+++ cogs/role_give.py
@@ -0,0 +1,40 @@
+from vote import vote, Importance
+import discord
+from discord.ext import commands
+from discord.guild import *
+from discord.utils import *
+
+class RoleGive(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    @commands.command(name='give_role')
+    async def give_role(self, 
+                    ctx : discord.TextChannel | discord.VoiceChannel | discord.StageChannel,
+                    id_name : str, 
+                    role : str):     
+        # Это часть кода возвращает либо роль, либо ничего, пожтому трай/експект тут не катит 
+        role_ = discord.utils.get(ctx.guild.roles, name=role)
+        if role_ == None:
+            await ctx.send("Роль не найдена")
+            return  
+        # Аналогично
+        member = discord.utils.get(ctx.guild.members, name=id_name)
+        if member == None:
+            await ctx.send("Юзер не найден")
+            return
+        else:
+            choice = await vote(self.bot, ctx, f"Дать ли пользователю {id_name} роль - {role}", ["Да", "Нет"], symbols='letters', importance=Importance.medium)
+            if choice.pop()==1:
+                await ctx.send("Голосование провалилось")
+                return
+            try:
+                # А вот эта часть кода уже может выдать ошибку. Так как бот не может присвоить роль выше своей (ну типо)
+                await member.add_roles(role_)  
+                await ctx.send("Роль выдана")    
+            except:
+                # Вот для этого тут и стоит это
+                await ctx.send("Операция невозможна")
+                return  
+
+async def setup(bot):
+    await bot.add_cog(RoleGive(bot))
--- main.py
+++ main.py
@@ -9,17 +9,8 @@ import asyncio
 TOKEN: Final[str|None] = os.getenv('BOT_TOKEN')
 assert TOKEN is not None, "Токен не найден, проверьте что переменная окружения $BOT_TOKEN содержит токен"
 
-#инициализация бота должна находиться в глобальной области видимости,
-#так как из main обьект класса Client никто не увидит
-INTENTS: Final[discord.Intents] = discord.Intents(
-    message_content=True, # TODO: App/hybrid commands since apparently no one else can figure them out
-    messages=True,
-    members=True,
-    guild_reactions=True,
-    typing=False,
-    presences=False,
-)
-bot = commands.Bot(command_prefix="/", intents=INTENTS)
+# это полное разрешение на все действия, иначе реализовать это походу не получится
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 # здесь не нужно ничего трогать
 async def load_extensions():
