3f8761214e6f82d552a56f8088859ada2a0afb78
AleksandrLisitsa <144948408+AleksandrLisitsa@users.noreply.github.com>
2024-04-15 10:08:58 +0300

    Add change_server_name.py, fix bug

---
+++ cogs/change_server_name.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from vote import vote, Importance
+
+class ServerManagement(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='change_server_name')
+    async def change_server_name(self, ctx, *, new_name: str):
+        choice = await vote(self.bot, ctx, f"Сменить имя сервера на {new_name}", ["Да", "Нет"], symbols='thumbs', importance=Importance.medium)
+        if choice.pop()==1:
+            await ctx.send("Голосование провалилось")
+            return
+        
+        else:
+            try:
+                await ctx.guild.edit(name=new_name)
+                await ctx.send(f'Имя сервера было успешно изменено на {new_name}')
+            except Exception as e:
+                await ctx.send(f'Произошла ошибка при изменении имени сервера: {e}')
+
+async def setup(bot):
+    await bot.add_cog(ServerManagement(bot))
