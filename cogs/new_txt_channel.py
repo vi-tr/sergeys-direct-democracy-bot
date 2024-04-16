0f596ca67ece0db290b328b09ed8340c4dc5d4b1
gingerrooty <145054356+gingerrooty@users.noreply.github.com>
2024-04-16 13:38:40 +0300

    create txt channel
    nado protestit'

---
+++ cogs/new_txt_channel.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+from vote import vote, Importance
+
+
+class NewTXTChannel(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def create_text_channel(self, ctx, category_name: str, channel_name: str):
+        guild = ctx.guild
+        category = discord.utils.get(guild.categories, name=category_name)
+
+        choice = await vote(bot, ctx, f"Создать ли текстовый канал {channel_name} в категории {category_name}",
+                            ["Да", "Нет"], symbols='thumbs', importance=Importance.medium)
+        if choice.pop() == 1:
+            await ctx.send("Голосование провалилось")
+            return
+        else:
+            if category is None:
+                category = await guild.create_category(category_name)
+                await ctx.send(f"Категория '{category_name}' создана.")
+
+            await guild.create_text_channel(channel_name, category=category)
+            await ctx.send(f"Текстовый канал '{channel_name}' успешно создан в категории '{category_name}'!")
+
+
+async def setup(bot):
+    await bot.add_cog(NewTXTChannel(bot))
