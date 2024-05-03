d729381884a2783166e1faa742493080a2c19d55
remr2005 <morpeh2005ka@gmail.com>
2024-05-03 17:53:25 +0300

    переделал один из тестов

--- tests/test_give_role.py
+++ tests/test_give_role.py
@@ -1,29 +1,31 @@
-import pytest
 import discord
-from discord.ext import commands
+import discord.ext.commands as commands
+from discord.ext.commands import Cog, command
+import pytest
+import pytest_asyncio
 import discord.ext.test as dpytest
 import os
-import asyncio
 
-os.chdir("/home/kemran/sergeys-direct-democracy-bot")
+@pytest_asyncio.fixture
+async def bot():
+    # Setup
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+    b = commands.Bot(command_prefix="/",
+                     intents=intents)
+    await b._async_setup_hook()  # setup the loop
+    await b.load_extension("cogs.role_give")
+
+    dpytest.configure(b)
+
+    yield b
+
+    # Teardown
+    await dpytest.empty_queue() # empty the global message queue as test teardown
+
 
 @pytest.mark.asyncio
-async def test_give_role():
-    INTENTS = discord.Intents(
-        message_content=True,
-        messages=True,
-        members=True,
-        guild_reactions=True,
-        guilds=True,
-        typing=False,
-        presences=False,
-        voice_states=True,
-    )
-    bot = commands.Bot(command_prefix="/", intents=INTENTS)
-    await bot._async_setup_hook()
-    await bot.load_extension("cogs.role_give")
-    dpytest.configure(bot)
+async def test_ping(bot):
     await dpytest.message("/test_give_role test_name test_role "+ os.getenv('BOT_TOKEN'))
     assert dpytest.verify().message().contains().content("Роль не найдена")
-
-asyncio.run(test_give_role())
