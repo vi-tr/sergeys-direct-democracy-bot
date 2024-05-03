5147cee8522325546dfaec26b5cd492d3c22eb0f
AleksandrLisitsa <144948408+AleksandrLisitsa@users.noreply.github.com>
2024-05-04 01:48:13 +0300

    fix

--- tests/test_serverbanner.py
+++ tests/test_serverbanner.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from unittest.mock import AsyncMock, MagicMock, mock_open, patch
 from discord.ext import commands
