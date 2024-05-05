04e4ce22c2f5a8789131dbf3bad409e0ed9e476b
vi-tr <144982881+vi-tr@users.noreply.github.com>
2024-05-05 20:42:48 +0300

    Add example test
    TODO: Figure out why voting hangs. client.wait_for deadlock?

---
+++ tests/test_example.py
@@ -0,0 +1,23 @@
+from main import INTENTS
+from cogs.example import SimpleCog
+from discord.ext import commands
+import pytest
+import pytest_asyncio
+import discord.ext.test as dt
+
+@pytest_asyncio.fixture
+async def bot():
+    # Setup
+    b = commands.Bot(command_prefix="/", intents=INTENTS)
+    await b._async_setup_hook()  # setup the loop
+    await b.add_cog(SimpleCog(b))
+    dt.configure(b, members=1)
+    yield b
+    # Teardown
+    await dt.empty_queue() # empty the global message queue as test teardown
+
+
+@pytest.mark.asyncio
+async def test_money(bot):
+    await dt.message("/lend_money")
+    assert dt.verify().message().content("Неа")
