1a4e2836579f1ea9d1b88a9228ce475d2978a73b
Kemran <109353270+remr2005@users.noreply.github.com>
2024-06-16 16:21:32 +0300

    Update roleradicator.py

--- cogs/roleradicator.py
+++ cogs/roleradicator.py
@@ -7,26 +7,6 @@ class RoleRemove(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='delete_role') #removing role completely
-    async def delete_role(self, ctx, role_name: str):
-        guild = ctx.guild
-        role = discord.utils.get(guild.roles, name=role_name)
-        if role is None:
-            await ctx.send(f'Роль {role_name} не найдена на сервере')
-            return
-        # execution (literally)
-        choice = await vote(self.bot, ctx, f"Уберём с сервера роль {role_name}?", ["Да", "Нет"], symbols='thumbs', importance=Importance.minor) #returned the standart medium value
-        if choice.pop() == 1:
-            await ctx.send("Голосование не подтверждено.")
-            return
-        try:
-            # eradication
-            await role.delete()
-            await ctx.send(f'Роль {role_name} удалена с сервера')
-        except discord.Forbidden:
-            await ctx.send("Я генетически непригоден для выполнения этой операции.")
-        except Exception as e:
-            await ctx.send(f"Ошибка при удалении роли: {e}")
 
     @commands.command(name='remove_role')
     async def remove_role(self, ctx, user_name: discord.Member, role_name: str):
